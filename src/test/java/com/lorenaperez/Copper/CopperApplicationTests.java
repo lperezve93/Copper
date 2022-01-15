8f2daeff74409347bcb348b00f9779660c17652c
lperezve93lperezve93lperezve <lorenap.uk93@gmail.com>
2022-01-15 20:30:59 +0000

    Initial commit

---
+++ src/test/java/com/lorenaperez/Copper/CopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenaperez.Copper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
